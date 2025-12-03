a4726496a423da8901de04cbaed7a84dbf6e8c32
Reinier Hernández <sasuke.reinier@gmail.com>
2025-12-03 16:45:48 -0300

    style: format React files for consistent style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react'
-import "@theme-toggles/react/css/Classic.css"
+import '@theme-toggles/react/css/Classic.css'
 import { Classic } from '@theme-toggles/react'
 import './App.css'
 
 function App() {
   const [size, setSize] = useState({ width: window.outerWidth, height: window.outerHeight })
-  const [isDark, setIsDark] = useState(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
+  const [isDark, setIsDark] = useState(
+    localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+  )
 
-  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  if (
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
     document.documentElement.classList.add('dark')
   } else {
     document.documentElement.classList.remove('dark')
@@ -36,16 +42,18 @@ function App() {
     }
   }, [])
   return (
-    <div className='transition duration-500 flex justify-center items-center w-screen h-screen bg-white dark:bg-gray-800 dark:text-gray-200'>
-      <h1 className='container text-center font-bold text-3xl'>
+    <div className="transition duration-500 flex justify-center items-center w-screen h-screen bg-white dark:bg-gray-800 dark:text-gray-200">
+      <h1 className="container text-center font-bold text-3xl">
         Your device is
-        <span className='block md:inline md:ml-1'>{size.width} x {size.height}</span>
+        <span className="block md:inline md:ml-1">
+          {size.width} x {size.height}
+        </span>
       </h1>
 
       <Classic
         toggle={setIsDark}
         toggled={!isDark}
-        className='absolute top-2 right-2 text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl'
+        className="absolute top-2 right-2 text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl"
         onToggle={switchMode}
         placeholder={isDark ? '🌙' : '☀️'}
         onPointerEnterCapture={undefined}
