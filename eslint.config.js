2c45094eaf6cad2bc23fe7fbbe96fb59909fc98b
Reinier Hernández <sasuke.reinier@gmail.com>
2026-02-24 10:22:09 -0300

    chore(eslint): migrate configuration to typescript-eslint preset

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,28 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from '@typescript-eslint/eslint-plugin'
-import tsparser from '@typescript-eslint/parser'
-import prettier from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
 
-export default [
-  {
-    ignores: ['dist', '.eslintrc.cjs']
-  },
+export default tseslint.config(
+  { ignores: ['dist'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: tsparser
     },
     plugins: {
-      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      ...prettierConfig.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ],
-      'prettier/prettier': 'error'
-    }
-  }
-]
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+  eslintPluginPrettier
+)
